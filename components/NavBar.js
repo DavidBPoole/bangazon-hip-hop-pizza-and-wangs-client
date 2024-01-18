03c7835f1ac10380f38854db781398d822535bc7
David Poole <dbpoole7@gmail.com>
2024-01-17 18:34:23 -0600

    homescreen view with links and navigation complete

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,8 +1,9 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import Link from 'next/link';
 import {
-  Navbar, //
+  Navbar,
   Container,
   Nav,
   Button,
@@ -14,20 +15,24 @@ export default function NavBar() {
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
         <Link passHref href="/">
-          <Navbar.Brand>CHANGE ME</Navbar.Brand>
+          <Navbar.Brand>
+            <img src="/HHPWLogo.png" width="auto%" height="100" alt="icon" className="nav-logo" />
+          </Navbar.Brand>
         </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
+          <Nav>
             {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
-            <Link passHref href="/">
-              <Nav.Link>Home</Nav.Link>
+            <Link passHref href="/orders">
+              <Nav.Link>View Orders</Nav.Link>
             </Link>
-            <Link passHref href="/delete-me">
-              <Nav.Link>Delete Me</Nav.Link>
+            <Link passHref href="/orders/new">
+              <Nav.Link>Create Order</Nav.Link>
             </Link>
-            <Button variant="danger" onClick={signOut}>
-              Sign Out
+          </Nav>
+          <Nav className="ms-auto"> {/* 'ms-auto' pushes the items to the right */}
+            <Button variant="danger" onClick={signOut} style={{ fontSize: 12 }}>
+              <b><em>Sign Out &#9916;</em></b>
             </Button>
           </Nav>
         </Navbar.Collapse>
